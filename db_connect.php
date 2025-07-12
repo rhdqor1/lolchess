aaa7c8011d1d8381e67081ae7115ae2d9ec362d0
heo <108280540+rhdqor1@users.noreply.github.com>
2025-07-12 23:47:30 +0900

    Update db_connect.sample.php

--- db_connect.php
+++ db_connect.php
@@ -3,7 +3,7 @@ putenv("NLS_LANG=KOREAN_KOREA.UTF8");
 $use_oracle = true;
 
 if ($use_oracle) {
-    $conn = oci_connect("dbuser212358", "ce1234", "//earth.gwangju.ac.kr:1521/orcl");
+   $conn = oci_connect("your_username", "your_password", "localhost/XE");
     if (!$conn) {
         $e = oci_error();
         die("Oracle DB 연결 실패: " . $e['message']);
